380a962f110bc5d48e24680c5cd01d649f89e5dd
AA22_Gi <gibert22@mail.ru>
2024-10-31 21:09:43 +0700

    написаны функции platform, shop, cart

--- UrbanDjango/task3/views.py
+++ UrbanDjango/task3/views.py
@@ -1,3 +1,23 @@
 from django.shortcuts import render
 
+
 # Create your views here.
+def platform(request):
+    return render(request, 'third_task/platform.html')
+
+
+def shop(request):
+    game1 = 'Atomic Heart'
+    game2 = 'Cyberpunk 2077'
+    game3 = 'PayDay 2'
+
+    games = {
+        game1: '2899 руб.',
+        game2: '3499 руб.',
+        game3: '1999 руб.',
+    }
+    return render(request, 'third_task/games_.html', {'games': games})
+
+
+def cart(request):
+    return render(request, 'third_task/cart.html')
