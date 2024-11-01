c0556076cd336866a5a30cd6931144e22f0e92d7
AA22_Gi <gibert22@mail.ru>
2024-11-02 01:06:04 +0700

    Изменена функция shop

--- UrbanDjango/task4/views.py
+++ UrbanDjango/task4/views.py
@@ -1,3 +1,19 @@
 from django.shortcuts import render
 
+
 # Create your views here.
+def platform(request):
+    return render(request, 'fourth_task/platform.html')
+
+
+def shop(request):
+    games = ['Atomic Heart', 'Cyberpunk 2077', 'PayDay 2']
+
+    context = {
+        'games': games
+    }
+    return render(request, 'fourth_task/games.html', context)
+
+
+def cart(request):
+    return render(request, 'fourth_task/cart.html')
